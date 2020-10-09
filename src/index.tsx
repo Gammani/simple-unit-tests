78e27c3c9c1997e86ad1339c007982e349067a1e
Oreda-Z <shtucer31@gmail.com>
2020-10-09 11:35:26 +0300

    init commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {splitIntoWords} from "./01-hello-tests/01";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const sentense = "Hello my friends!"
+const result = splitIntoWords(sentense);
+console.log(result);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
